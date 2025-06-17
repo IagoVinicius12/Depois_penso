d998b50674791a5871e3da3e8bfe8062ee3f7833
IagoVinicius12 <erolnazori@gmail.com>
2025-06-17 16:49:16 -0300

    começando

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import viteLogo from "../../public/vite.svg"
 
 function Home(){
     return(
         <div>
-            
+            <section>
+                <img src={viteLogo} alt="vite.svg" />
+            </section>
+            <section>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            </section>
         </div>
     )
 }
