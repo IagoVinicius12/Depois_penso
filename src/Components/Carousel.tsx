1c7c75ae8c61fd885fa8317de73ddd6c47e62612
IagoVinicius12 <erolnazori@gmail.com>
2025-06-22 23:50:22 -0300

    sla

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
+
 export default function Carousel(){
     const imagens=[
         '/SLA.jpg'
@@ -13,8 +14,17 @@ export default function Carousel(){
     const handlePrev = () =>{
         setIndex(index-1)
     }
+    const visibleImages = imagens.slice(index, index + 3);
     return(
         <div>
+            {visibleImages.map((src,index)=>(
+                <section style={{width:"15%", backgroundColor:"white", borderRadius:15 }}>
+                    <img key={index} src={src} alt="" style={{width:'100%', borderTopRightRadius:15, borderTopLeftRadius:15}} />
+                    <div style={{display:"flex", padding:20}}>
+                        Some quick example text to build on the card title and make up the bulk of the card’s content.
+                    </div>
+                </section>
+            ))}
         </div>
     )
 }
