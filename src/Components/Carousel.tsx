c08ad8518b575237e8a2ca814cafdfd7748328ee
IagoVinicius12 <erolnazori@gmail.com>
2025-06-23 17:53:35 -0300

    sla

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,30 +1,55 @@
 import React from "react";
 import { useState } from "react";
+import { motion } from 'framer-motion';
+import './carousel.css'
 
 
-export default function Carousel(){
-    const imagens=[
-        '/SLA.jpg'
+
+export default function Carousel() {
+    const imagens = [
+        '../../Images/SLA.jpg',
+        '../../Images/SLA1.jpg',
+        '../../Images/SLA2.jpg',
+        '../../Images/SLA3.jpg',
     ];
-    const [index, setIndex]=useState(0);
-    
-    const handleNext = () =>{
-        setIndex(index+1)
+    const texts = [
+        'aaaaaa',
+        'bbbbbbb',
+        'cccccccccc',
+        'sddddddddd'
+    ]
+    const [index, setIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(index);
+    const [change, setChange] = useState(false)
+
+    const handleNext = () => {
+        setIndex(index + 1)
+        setChange(true)
+        setTimeout(() => {
+            setChange(false);
+        }, 400);
     }
-    const handlePrev = () =>{
-        setIndex(index-1)
+    const handlePrev = () => {
+        setIndex(index - 1)
+        setChange(true)
+        setTimeout(() => {
+            setChange(false);
+        }, 400);
     }
     const visibleImages = imagens.slice(index, index + 3);
-    return(
-        <div>
-            {visibleImages.map((src,index)=>(
-                <section style={{width:"15%", backgroundColor:"white", borderRadius:15 }}>
-                    <img key={index} src={src} alt="" style={{width:'100%', borderTopRightRadius:15, borderTopLeftRadius:15}} />
-                    <div style={{display:"flex", padding:20}}>
-                        Some quick example text to build on the card title and make up the bulk of the card’s content.
+    const visibleTexts = texts.slice(index, index + 3)
+    return (
+        <div style={{ display: "flex", backgroundColor: "black", justifyContent: "center", alignItems: "center", height: '37%' }}>
+            <button onClick={handlePrev} disabled={index <= 0} style={{ backgroundColor: index > 0 ? 'gray' : 'blue', width: '2%', height: '20%' }}></button>
+            {imagens.map((src, index) => (index >= currentIndex && index <= currentIndex + 2 ? (
+                <section key={index} style={{ width: "15%", backgroundColor: "white", borderRadius: 15 }} className={change == true ? 'diminuir_aumentar' : 'none'}>
+                    <img key={index} src={src} alt="" style={{ width: '100%', borderTopRightRadius: 15, borderTopLeftRadius: 15 }} />
+                    <div style={{ display: "flex", padding: 20 }}>
+                        {visibleTexts[index]}
                     </div>
                 </section>
-            ))}
+            ) : null))}
+            <button onClick={handleNext} disabled={index == visibleImages.length - 2} style={{ backgroundColor: 'gray', width: '2%', height: '20%' }}></button>
         </div>
     )
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import viteLogo from "../../public/vite.svg"
 import gitLogo from '../../github.svg'
 import linkedinLogo from '../../linkedin.svg'
-import Sla from '../../SLA.jpg'
+import Sla from '../../Images/SLA.jpg'
+import Carousel from "../Components/Carousel";
 
 function Home() {
     return (
-        <div style={{ justifyContent: "center", height: "100vh", width: '100vw' }}>
+        <div style={{ justifyContent: "center", height: "100vh", width: '100vw',overflowX:'hidden', flexDirection:'column' }}>
             <div style={{ display: "flex", justifyContent: "center", height: '25%' }}>
                 <div style={{ paddingTop: '5%', display: "flex", width: '40%', height: '100%', justifyContent: "center", backgroundColor: 'red' }}>
                     <div className='1' style={{ display: "flex", flexDirection: "column", width: '30%', alignItems: "center", height: '100%' }}>
                         <section style={{ display: 'flex', backgroundColor: 'gray', justifyContent: "end", paddingRight: '5%', width: '100%', height: '100%' }}>
                             <img src={viteLogo} alt="vite.svg" style={{ borderRadius: 50, backgroundColor: "black", width: '40%' }} />
                         </section>
-                        <section style={{ display: "flex", backgroundColor: 'green', justifyContent: "end",alignItems:"center", paddingRight: '5%', width: '100%', height: '100%' }}>
+                        <section style={{ display: "flex", backgroundColor: 'green', justifyContent: "end", alignItems: "center", paddingRight: '5%', width: '100%', height: '100%' }}>
                             <a href="https://github.com/IagoVinicius12" target="_blank"><img src={gitLogo} alt="github.svg" style={{ width: '40px' }} /></a>
                             <a href="https://www.linkedin.com/in/iago-vin%C3%ADcius-39b675304/" target="_blank"><img src={linkedinLogo} alt="linkedin.svg" style={{ width: '40px' }} /></a>
                         </section>
@@ -26,18 +27,24 @@ function Home() {
             <div style={{ display: "flex", justifyContent: "center", width: '100%', alignContent: "flex-start" }}>
                 <hr style={{ width: '50%' }} />
             </div>
-            <div style={{display:"flex", justifyContent:"center"}}>
+            <div style={{flex:1, height:'7%'}}></div>
+            <div style={{ display: "flex", justifyContent: "center"}}>
                 <h2>
                     Projetos Pessoais
                 </h2>
             </div>
-            <div style={{ display: "flex", width: "100%", justifyContent: "center", backgroundColor: 'black' }}>
-                <section style={{width:"15%", backgroundColor:"white", borderRadius:15 }}>
-                    <img src={Sla} alt="" style={{width:'100%', borderTopRightRadius:15, borderTopLeftRadius:15}} />
-                    <div style={{display:"flex", padding:20}}>
-                        Some quick example text to build on the card title and make up the bulk of the card’s content.
-                    </div>
+            <Carousel></Carousel>
+            <div style={{flex:1, height:'7%'}}></div>
+            <div style={{display:"flex",justifyContent:"center",width:'100%'}}>
+                <section style={{display:"flex", width:'60%', justifyContent:"start"}}><a>
+                    Sobre Mim</a></section>
+            </div>
+            <div style={{display:"flex", width:'100%',height:'40%', justifyContent:"center", alignItems:"center", gap:'20px', paddingTop:'20px'}}>
+                <img src={Sla} alt="" style={{width:'30%', height:'100%', borderRadius:25}} />
+                <section style={{display:"flex",width:'30%',height:'100%',alignItems:'start'}}>
+                    ERUGFHUQIEBRGJIHARGJBN HRIAHGUIO4RH HGQERTHUIGHQ UIEHuiergh ue4rhgu9    qh4erg 9    uqgihr59iueqh
                 </section>
+
             </div>
         </div>
     )
