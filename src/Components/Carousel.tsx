a62f603a31eccae7405248611d92c93325ff3563
IagoVinicius12 <erolnazori@gmail.com>
2025-06-26 17:43:57 -0300

    sla

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react"; 
+
+import { useState } from "react";
 import './carousel.css'
+
+
 
 export default function Carousel() {
     const imagens = [
@@ -13,13 +16,13 @@ export default function Carousel() {
         'bbbbbbb',
         'cccccccccc',
         'sddddddddd'
-    ];
+    ]
     const [index, setIndex] = useState(0);
-    const [currentIndex, setCurrentIndex] = useState(index);
-    const [change, setChange] = useState(false);
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+    const [currentIndex] = useState(index);
+    const [change, setChange] = useState(false)
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
 
-     const handleNext = () => {
+    const handleNext = () => {
         setIndex(index + 1)
         setChange(true)
         setTimeout(() => {
@@ -33,33 +36,29 @@ export default function Carousel() {
             setChange(false);
         }, 400);
     }
-
+    const visibleTexts = texts.slice(index, index + 3)
     return (
-        <div style={{ display: "flex", backgroundColor: "black", justifyContent: "center", alignItems: "center", height: '37%', gap:'5px'}}>
+        <div style={{ display: "flex", backgroundColor: "black", justifyContent: "center", alignItems: "center", height: '37%' ,gap:'5px'}}>
             <button onClick={handlePrev} disabled={index <= 0} style={{ backgroundColor: index > 0 ? 'gray' : 'blue', width: '2%', height: '20%' }}></button>
-            {imagens.map((src, idx) => (idx >= currentIndex && idx <= currentIndex + 2 ? (
-                <section
-                  key={idx}
-                  style={{
-                    width: "15%",
-                    backgroundColor: "white",
-                    borderRadius: 15,
-                    transform: hoveredIndex === idx ? "scale(1.05)" : "scale(1)",
-                    transition: "transform 0.3s ease-in-out",
-                    zIndex: hoveredIndex === idx ? 1 : 0
-                  }}
-                  className={change ? 'diminuir_aumentar' : 'none'}
-                  onMouseEnter={() => setHoveredIndex(idx)}
-                  onMouseLeave={() => setHoveredIndex(null)}
-                >
-                  <img src={src} alt="" style={{ width: '100%', borderTopRightRadius: 15, borderTopLeftRadius: 15 }} />
-                  <div style={{ display: "flex", padding: 20 }}>
-                    {texts[idx]}
-                  </div>
+            {imagens.map((src, index) => (index >= currentIndex && index <= currentIndex + 2 ? (
+                <section 
+                key={index}
+                style={{ width: "15%", 
+                backgroundColor: "white", 
+                borderRadius: 15, 
+                transform: hoveredIndex === index ? "scale(1.05)" : "scale(1)", 
+                transition: "transform 0.3s ease-in-out", 
+                zIndex: hoveredIndex === index ? 1 : 0 }} 
+                className={change == true ? 'diminuir_aumentar' : 'none'} 
+                onMouseEnter={() => setHoveredIndex(index)} 
+                onMouseLeave={()=>setHoveredIndex(null)}>
+                    <img key={index} src={src} alt="" style={{ width: '100%', borderTopRightRadius: 15, borderTopLeftRadius: 15 }} />
+                    <div style={{ display: "flex", padding: 20 }}>
+                        {visibleTexts[index]} {index}
+                    </div>
                 </section>
             ) : null))}
-            <button onClick={handleNext} disabled={index === imagens.length - 3} style={{ backgroundColor: 'gray', width: '2%', height: '20%' }}></button>
+            <button onClick={handleNext} disabled={index == imagens.length - 3} style={{ backgroundColor: 'gray', width: '2%', height: '20%' }}></button>
         </div>
-    );
+    )
 }
-
